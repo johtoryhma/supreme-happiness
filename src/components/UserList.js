61f90fff8160603c7f2c0305346bf28d11930fae
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-08 10:43:35 +0300

    Added UserList component

---
+++ src/components/UserList.js
@@ -0,0 +1,11 @@
+const UserList = ({ users }) => {
+  return (
+    <ul>
+      {users.map((user) => {
+        return <li key={user.id}>{user.name}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default UserList;
