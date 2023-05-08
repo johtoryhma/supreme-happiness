90abed7ebf1dd07260ed5b7ec50a391b6b2aead5
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-08 11:29:32 +0300

    name has state on form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import UserList from "./components/UserList";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [users] = useState([
     { id: 1, name: "kissa", role: "tank" },
     { id: 2, name: "koira", role: "healer" },
   ]);
+  const [formUser, setFormUser] = useState({ name: "", role: "" });
 
   return (
     <div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,30 @@
-const Form = () => {
-    return (
-        <form>
-            <label for="test1">
-                Test1
-                <input name="test1">
-                </input>
-                </label>
-            <label for="textarea">
-                Insert text:
-                <input type="text" name="textarea"></input>
-            </label>
-        </form>
-    );
-}
+import { useState } from "react";
+
+const Form = ({ save }) => {
+  const [name, setName] = useState("");
+
+  const changeName = (event) => {
+    console.log("nimi on nyt: ", event.target.value);
+    setName(event.target.value);
+  };
+  const handleClick = () => {
+    // check for validity
+    //save(user);
+  };
+
+  return (
+    <form>
+      <label for="name">
+        Name
+        <input name="name" value={name} onChange={changeName}></input>
+      </label>
+      <label for="textarea">
+        Insert text:
+        <input type="text" name="textarea"></input>
+      </label>
+      <button onClick={handleClick}>Save</button>
+    </form>
+  );
+};
 
 export default Form;
