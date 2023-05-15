bd93ef6296ee78a514b9a4f6c051cd1502b54604
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-15 13:01:32 +0300

    changed style for prettier

--- src/App.js
+++ src/App.js
@@ -21,12 +21,14 @@ function App() {
 
   return (
     <div>
-      <h1>Hello!</h1>
-      <div className="list-container">
-        <UserList users={users} />
-      </div>
-      <div className="form-container">
-        <Form users={users} save={handleSave} />
+      <h1>Gamer stats</h1>
+      <div className="main-container">
+        <div className="list-container">
+          <UserList users={users} />
+        </div>
+        <div className="form-container">
+          <Form users={users} save={handleSave} />
+        </div>
       </div>
     </div>
   );
