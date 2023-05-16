af0e85d4e82fbc3e1da5b54b1050b4688ea814f6
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-16 14:47:13 +0300

    deleted unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Form from "./components/Form";
 import UserList from "./components/UserList";
 import Statbox from "./components/Statbox";
