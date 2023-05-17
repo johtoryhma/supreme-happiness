37f3dd1f39e2f11ec4c26a051d25cee39ca08ef6
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-17 11:25:40 +0300

    added user-card when hovering over name
    Co-authored-by: Jalmari Hänninen <jalmarihanninen@users.noreply.github.com>

--- src/components/UserLi.js
+++ src/components/UserLi.js
@@ -1,8 +1,10 @@
+import UserName from "./UserName";
+
 const UserLi = ({ user }) => {
   let joinDate = new Date(Date.parse(user.joinDate));
   return (
     <li className="list-item" key={user.id}>
-      <i className="name-on-list">{user.name}</i> usually plays{" "}
+      <UserName user={user} /> usually plays{" "}
       <i className="role-on-list">{user.role}</i>, joined{" "}
       <i className="date-on-list">{`${joinDate.getDay()}.${joinDate.getMonth()}.${joinDate.getFullYear()}`}</i>
     </li>
