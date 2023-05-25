3bb3ab54f4fd54da130a343ab3ec6baafccaee70
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-25 13:55:55 +0300

    added handleClickOutside to UserName

--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,10 +1,30 @@
 import { BigHead } from "@bigheads/core";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const UserName = ({ user }) => {
   const [show, setShow] = useState(false);
+  const [clicked, setClicked] = useState(false);
   const [cardPosition, setCardPosition] = useState({ top: 0, left: 0 });
   const cardRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (cardRef.current && !cardRef.current.contains(event.target)) {
+      setClicked(false);
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log("clicked?", clicked);
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [clicked]);
+
+  const handleClicked = (event) => {
+    setClicked(true);
+  };
 
   const showCard = (e) => {
     const cardRect = cardRef.current.getBoundingClientRect();
@@ -19,13 +39,18 @@ const UserName = ({ user }) => {
   };
 
   return (
-    <i className="name-on-list" onMouseEnter={showCard} onMouseLeave={hideCard}>
+    <i
+      className="name-on-list"
+      onClick={handleClicked}
+      onMouseEnter={showCard}
+      onMouseLeave={hideCard}
+    >
       {user.name.length > 15 ? user.name.slice(0, 15) + "..." : user.name}
       <div
         ref={cardRef}
         className="user-card"
         style={
-          show
+          clicked || show
             ? {
                 visibility: "visible",
                 top: cardPosition.top,
@@ -34,30 +59,27 @@ const UserName = ({ user }) => {
             : {}
         }
       >
-        <div>
-          User is <div className="name-on-list">{user.name}</div> and looks like
-          this:
-          <BigHead
-            accessory={user.avatar.accessory}
-            body={user.avatar.body}
-            circleColor={user.avatar.circleColor}
-            clothing={user.avatar.clothing}
-            clothingColor={user.avatar.clothingColor}
-            eyebrows={user.avatar.eyebrows}
-            eyes={user.avatar.eyes}
-            facialHair={user.avatar.facialHair}
-            graphic={user.avatar.graphic}
-            hair={user.avatar.hair}
-            hairColor={user.avatar.hairColor}
-            hat={user.avatar.hat}
-            hatColor={user.avatar.hatColor}
-            lashes={user.avatar.lashes}
-            lipColor={user.avatar.lipColor}
-            mask="true"
-            mouth={user.avatar.mouth}
-            skinTone={user.avatar.skinTone}
-          />
-        </div>
+        User is {user.name} and looks like this:
+        <BigHead
+          accessory={user.avatar.accessory}
+          body={user.avatar.body}
+          circleColor={user.avatar.circleColor}
+          clothing={user.avatar.clothing}
+          clothingColor={user.avatar.clothingColor}
+          eyebrows={user.avatar.eyebrows}
+          eyes={user.avatar.eyes}
+          facialHair={user.avatar.facialHair}
+          graphic={user.avatar.graphic}
+          hair={user.avatar.hair}
+          hairColor={user.avatar.hairColor}
+          hat={user.avatar.hat}
+          hatColor={user.avatar.hatColor}
+          lashes={user.avatar.lashes}
+          lipColor={user.avatar.lipColor}
+          mask="true"
+          mouth={user.avatar.mouth}
+          skinTone={user.avatar.skinTone}
+        />
       </div>
     </i>
   );
